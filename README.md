7fad9badc85436f4c0c52ed357543cd724aad722
ramses2099 <ramses2099@gmail.com>
2025-04-04 03:41:13 -0400

    add new extension region for vscode

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# project-leetcode
+# Project Leetcode
 Leetcode tutorial in rust
+
+## extension #region folding for VS Code
+
+```
+The extension also installs a command to wrap a region comment around the current selection.
+
+    regionfolder.wrapWithRegion (Ctrl+M Ctrl+R)
+```
+## example #region
+```
+  /* #region two sum */   
+    pub fn two_sum_bruteforce(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for (i, num) in nums.iter().enumerate() {
+            // println!("index {}, {} value", i, num);
+            for (j, num2) in nums
+                .iter()
+                .skip(i + 1)
+                .enumerate() {
+                // println!("index {}, {} value", j, num2);
+                if num + num2 == target {
+                    return vec![i as i32, (j + 1 + i) as i32];
+                }
+            }
+        }
+
+        vec![]
+    }
+    
+    /* #endregion */
+```
--- leetcode-lib/src/lib.rs
+++ leetcode-lib/src/lib.rs
@@ -1,6 +1,7 @@
 pub struct Solution;
 
 impl Solution {
+    /* #region two sum */   
     pub fn two_sum_bruteforce(nums: Vec<i32>, target: i32) -> Vec<i32> {
         for (i, num) in nums.iter().enumerate() {
             // println!("index {}, {} value", i, num);
@@ -17,6 +18,24 @@ impl Solution {
 
         vec![]
     }
+   
+    pub fn two_sum_hashmap(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for (i, num) in nums.iter().enumerate() {
+            // println!("index {}, {} value", i, num);
+            for (j, num2) in nums
+                .iter()
+                .skip(i + 1)
+                .enumerate() {
+                // println!("index {}, {} value", j, num2);
+                if num + num2 == target {
+                    return vec![i as i32, (j + 1 + i) as i32];
+                }
+            }
+        }
+
+        vec![]
+    }
+    /* #endregion */
 }
 
 #[cfg(test)]
