d9deb9c7e4c588ae902cf43e13c54e38de8103d6
ramses2099 <ramses2099@gmail.com>
2025-04-05 12:14:33 -0400

    sum two list reverse

--- leetcode-lib/src/lib.rs
+++ leetcode-lib/src/lib.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
+
 pub struct Solution;
 
 impl Solution {
-    /* #region two sum */   
+    /* #region two sum */
     pub fn two_sum_bruteforce(nums: Vec<i32>, target: i32) -> Vec<i32> {
         for (i, num) in nums.iter().enumerate() {
             // println!("index {}, {} value", i, num);
@@ -18,19 +20,16 @@ impl Solution {
 
         vec![]
     }
-   
+
     pub fn two_sum_hashmap(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut hash: HashMap<i32, usize> = HashMap::new();
+
         for (i, num) in nums.iter().enumerate() {
-            // println!("index {}, {} value", i, num);
-            for (j, num2) in nums
-                .iter()
-                .skip(i + 1)
-                .enumerate() {
-                // println!("index {}, {} value", j, num2);
-                if num + num2 == target {
-                    return vec![i as i32, (j + 1 + i) as i32];
-                }
+            let complement = target - num;
+            if hash.contains_key(&complement) {
+                return vec![hash[&complement] as i32, i as i32];
             }
+            hash.insert(*num, i);
         }
 
         vec![]
@@ -47,6 +46,14 @@ mod tests {
         let nums = vec![2, 7, 11, 15];
         let target = 9;
         let result = Solution::two_sum_bruteforce(nums, target);
+        assert_eq!(result, vec![0, 1]);
+    }
+
+    #[test]
+    fn test_two_sum_hash_map() {
+        let nums = vec![2, 7, 11, 15];
+        let target = 9;
+        let result = Solution::two_sum_hashmap(nums, target);
         assert_eq!(result, vec![0, 1]);
     }
 }
--- leetcode-ui/src/main.rs
+++ leetcode-ui/src/main.rs
@@ -1,9 +1,33 @@
+use std::collections::LinkedList;
+
+type List = LinkedList<i32>;
+
+fn add_two_numbers(l1: List, l2: List) -> List {
+    let mut l1 = l1;
+    let mut iter = l1.iter_mut();
+    // for x in iter {
+    //     print!("{:?}", *x);
+    // }
+    while let Some(x) = iter.next() {
+        println!("{}", *x);
+    }
+    l2
+}
+
 fn main() {
-    let nums = vec![2, 7, 11, 15];
-    println!("Vec: {:?}", nums);
-    let target = 9;
-    println!("Target: {}", target);
-    let result = leetcode_lib::Solution::two_sum_bruteforce(nums, target);
-    assert_eq!(result, vec![0,1]);
-    println!("Indices: {:?}", result);
+    let mut list1: LinkedList<i32> = LinkedList::from([2,4,3]);
+    println!("List 1: {:?}", list1);
+
+    let mut list2: LinkedList<i32> = LinkedList::from([5,6,4]);
+    println!("List 2: {:?}", list2);
+
+    let mut iter1 = list1.iter().rev();
+    let mut iter2 = list2.iter().rev();
+    let mut result: LinkedList<i32> = LinkedList::new();
+
+    while let (Some(x), Some(y)) = (iter1.next(), iter2.next()) {
+        let sum = *x + *y;
+        result.push_back(sum);
+    }
+    println!("Result: {:?}", result);
 }
